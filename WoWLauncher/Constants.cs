628efc34b0cd4576ef9eadd1af3ad4a43fc19048
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-09-27 04:29:00 +0200

    Resize + web french uri + web external browser + clean nuget + cleaning

--- WoWLauncher/Constants.cs
+++ WoWLauncher/Constants.cs
@@ -2,6 +2,8 @@
 {
     public static class Constants
     {
-        public const string WebFrameUriToLoad = "https://worldofwarcraft.blizzard.com/news";
+        public const string WebFrameUriToLoad = "https://worldofwarcraft.blizzard.com/en-us/news";
+
+        public const string WebFrameUriToLoadFr = "https://worldofwarcraft.blizzard.com/fr-fr/news";
     }
 }
--- WoWLauncher/MainWindow.cs
+++ WoWLauncher/MainWindow.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Web.WebView2.Core;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
@@ -13,16 +14,60 @@ namespace WoWRetroLauncher
     {
         private SoundPlayer soundPlayer;
         public string Locale;
+        private const int WmSizing = 0x0214;
+        private readonly float targetRatio;
+        private Size originalFormSize;
+        private readonly Dictionary<Control, Rectangle> originalControlBounds;
+        private readonly Dictionary<Control, float> originalFontSizes;
+        private double originalWebViewZoom;
+
+        [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
+        private struct Rect
+        {
+            public int Left, Top, Right, Bottom;
+        }
 
         public MainWindow()
         {
+            Locale = Helper.LoadLocale();
+
             InitializeComponent();
-            this.Load += MainWindow_Load;
+
+            targetRatio = (float)this.ClientSize.Width / this.ClientSize.Height;
+            this.MaximizeBox = false;
+
+            originalControlBounds = new Dictionary<Control, Rectangle>();
+            originalFontSizes = new Dictionary<Control, float>();
+            originalWebViewZoom = 1.0;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WmSizing)
+            {
+                var rc = (Rect)System.Runtime.InteropServices.Marshal.PtrToStructure(m.LParam, typeof(Rect));
+                var width = rc.Right - rc.Left;
+                var newHeight = (int)(width / targetRatio);
+                rc.Bottom = rc.Top + newHeight;
+                System.Runtime.InteropServices.Marshal.StructureToPtr(rc, m.LParam, true);
+            }
+
+            base.WndProc(ref m);
         }
 
         private async void MainWindow_Load(object sender, EventArgs e)
         {
+            originalFormSize = this.ClientSize;
+
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is ToolStrip || ctrl is MenuStrip) continue;
+                originalControlBounds[ctrl] = ctrl.Bounds;
+                originalFontSizes[ctrl] = ctrl.Font.Size;
+            }
+
             await InitWebView();
+            OnLoad(sender, e);
         }
 
         private async Task InitWebView()
@@ -33,20 +78,43 @@ namespace WoWRetroLauncher
                 var env = await CoreWebView2Environment.CreateAsync(userDataFolder: tempFolder);
                 await webView21.EnsureCoreWebView2Async(env);
 
-                _ = Task.Run(() =>
+                originalWebViewZoom = webView21.ZoomFactor;
+
+                webView21.CoreWebView2.NewWindowRequested += (s, args) =>
                 {
-                    webView21.Invoke(new Action(() =>
+                    args.Handled = true;
+                    Process.Start(new ProcessStartInfo(args.Uri) { UseShellExecute = true });
+                };
+
+                webView21.CoreWebView2.NewWindowRequested += (s, args) =>
+                {
+                    args.Handled = true;
+                    if (!string.IsNullOrEmpty(args.Uri))
                     {
-                        var webPageToDisplay = Constants.WebFrameUriToLoad ?? string.Empty;
-                        if (!string.IsNullOrEmpty(webPageToDisplay) && Helper.ConnectionAlive(webPageToDisplay))
-                            webView21.Source = new Uri(webPageToDisplay);
-                        else
-                        {
-                            webView21.Visible = false;
-                            webView21.Enabled = false;
-                        }
-                    }));
-                });
+                        Process.Start(new ProcessStartInfo(args.Uri) { UseShellExecute = true });
+                    }
+                };
+
+                webView21.CoreWebView2.NavigationStarting += (s, args) =>
+                {
+                    // Do nothing
+                };
+
+                string webPageToDisplay;
+                if (Locale == "frFR")
+                    webPageToDisplay = !string.IsNullOrEmpty(Constants.WebFrameUriToLoadFr) ? Constants.WebFrameUriToLoadFr : Constants.WebFrameUriToLoad;
+                else
+                    webPageToDisplay = !string.IsNullOrEmpty(Constants.WebFrameUriToLoad) ? Constants.WebFrameUriToLoad : string.Empty;
+
+                if (!string.IsNullOrEmpty(webPageToDisplay) && Helper.ConnectionAlive(webPageToDisplay))
+                {
+                    webView21.Source = new Uri(webPageToDisplay);
+                }
+                else
+                {
+                    webView21.Visible = false;
+                    webView21.Enabled = false;
+                }
             }
             catch
             {
@@ -55,11 +123,53 @@ namespace WoWRetroLauncher
             }
         }
 
+        private void MainWindow_Resize(object sender, EventArgs e)
+        {
+            try
+            {
+                var targetRatio = (float)originalFormSize.Width / originalFormSize.Height;
+                var newWidth = this.ClientSize.Width;
+                var newHeight = (int)(newWidth / targetRatio);
+
+                if (this.ClientSize.Height != newHeight)
+                {
+                    this.ClientSize = new Size(newWidth, newHeight);
+                    return;
+                }
+
+                var ratio = (float)this.ClientSize.Width / originalFormSize.Width;
+
+                foreach (var kvp in originalControlBounds)
+                {
+                    var ctrl = kvp.Key;
+                    var r = kvp.Value;
+
+                    ctrl.SetBounds(
+                        Math.Max(1, (int)(r.Left * ratio)),
+                        Math.Max(1, (int)(r.Top * ratio)),
+                        Math.Max(1, (int)(r.Width * ratio)),
+                        Math.Max(1, (int)(r.Height * ratio))
+                    );
+
+                    if (originalFontSizes.TryGetValue(ctrl, out var s))
+                    {
+                        var newFontSize = Math.Max(1, s * ratio);
+                        ctrl.Font = new Font(ctrl.Font.FontFamily, newFontSize, ctrl.Font.Style);
+                    }
+                }
+
+                webView21.ZoomFactor = (float)(originalWebViewZoom * ratio);
+            }
+            catch
+            {
+                // Do nothing
+            }
+        }
+
         private void OnLoad(object sender, EventArgs e)
         {
             try
             {
-                Locale = Helper.LoadLocale();
                 if (Locale == "frFR")
                 {
                     this.buttonPlay.Name = "Jouer";
@@ -77,7 +187,7 @@ namespace WoWRetroLauncher
                     this.gameDetailsLarge.Text = @"World of Warcraft" + Environment.NewLine + realmDetails;
                     this.Text = @"World of Warcraft Launcher";
                 }
-                else if(string.IsNullOrEmpty(versionDetails) && string.IsNullOrEmpty(realmDetails))
+                else if (string.IsNullOrEmpty(versionDetails) && string.IsNullOrEmpty(realmDetails))
                 {
                     this.gameDetailsShort.Text = @"World of Warcraft Launcher";
                     this.Text = @"World of Warcraft Launcher";
@@ -103,7 +213,6 @@ namespace WoWRetroLauncher
         {
             try
             {
-                
                 var currentDir = AppDomain.CurrentDomain.BaseDirectory;
                 string[] candidates = { "Wow-64.exe", "Wow.exe" };
                 soundPlayer.Play();
